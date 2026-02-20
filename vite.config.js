c52edcb7378e4fd6b80275d08016f78a1718162d
LeonisouzaM <34925982+LeonisouzaM@users.noreply.github.com>
2026-02-20 19:07:38 -0300

    salvando antes do pull

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: {
-        minify: 'terser',
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
+        minify: 'esbuild',
         reportCompressedSize: false, // Speed up build
         rollupOptions: {
             output: {
